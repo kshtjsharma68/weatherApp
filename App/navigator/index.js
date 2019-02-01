087d5cbcb3880884d93598b1b76e51c3a8ab7e1a
kshitij sharma <kshtjsharma68@gmail.com>
2019-02-01 16:17:06 +0530

    navigator with comments

--- App/navigator/index.js
+++ App/navigator/index.js
@@ -1,10 +1,13 @@
 import { createStackNavigator, createAppContainer  } from 'react-navigation';
 import App from '../index';
+/**
+* Screen's or components used for navigator
+*/
 import Index from '../appIndex';
 import Weather from '../weatherIndex';
 import News from '../newsIndex';
 
-
+//Stack navigator
 const mainNav = createStackNavigator({
 	Home:{screen: Index},
 	Weather:{screen: Weather},
@@ -13,6 +16,7 @@ const mainNav = createStackNavigator({
 	initialRouteName: "Home"
 });
 
+// createAppContainer:  used to create Wrapper for stacknavigator when using with navigation on app component.
 const nav = createAppContainer(mainNav);
 
 export default nav;     
